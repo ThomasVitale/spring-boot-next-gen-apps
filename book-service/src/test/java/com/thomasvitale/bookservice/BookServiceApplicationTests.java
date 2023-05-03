b9d80a9c57c839c126de4971a3012933377788b8
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-05-03 23:32:19 +0200

    Upgrade to Spring Boot 3.1 RC1

--- book-service/src/test/java/com/thomasvitale/bookservice/BookServiceApplicationTests.java
+++ book-service/src/test/java/com/thomasvitale/bookservice/BookServiceApplicationTests.java
@@ -1,17 +1,16 @@
 package com.thomasvitale.bookservice;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = "spring.datasource.url=jdbc:tc:postgresql:15:///")
+@Import(TestEnvironmentConfiguration.class)
 class BookServiceApplicationTests {
 
 	@Autowired
--- order-service/src/main/java/com/thomasvitale/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/thomasvitale/orderservice/OrderServiceApplication.java
@@ -5,9 +5,6 @@ import java.time.Duration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Mono;
-import reactor.util.retry.Retry;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -19,18 +16,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClient.Builder;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
