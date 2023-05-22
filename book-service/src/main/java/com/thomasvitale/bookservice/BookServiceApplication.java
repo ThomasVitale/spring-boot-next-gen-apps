07e7b57fb07387f31f2da1dbc29540d8f07af8f0
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-05-22 23:15:42 +0200

    Update java begin

--- book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
@@ -1,12 +1,7 @@
 package com.thomasvitale.bookservice;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BookServiceApplication {
@@ -16,19 +11,3 @@ public class BookServiceApplication {
 	}
 
 }
-
-@RestController
-@RequestMapping("/books")
-class BookController {
-
-	@GetMapping
-	List<Book> getBooks() {
-		return List.of(
-			new Book(1L,"The Hobbit"),
-			new Book(2L, "The Lord of the Rings")
-		);
-	}
-
-}
-
-record Book(Long id, String title){}
